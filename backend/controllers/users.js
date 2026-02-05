f860b1206957f5dd188223e9924ed88693e82b12
tttom3669 <tttom3669@gmail.com>
2026-02-05 22:25:34 +0800

    fix: 更新密碼規則提示，要求密碼長度至少為 10 個字

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -29,10 +29,10 @@ class UsersController {
         return
       }
       if (!passwordPattern.test(password)) {
-        logger.warn('建立使用者錯誤: 密碼不符合規則，需要包含英文數字大小寫，最短10個字，最長16個字')
+          logger.warn('建立使用者錯誤: 密碼不符合規則，需要包含英文數字大小寫，最短10個字，最長16個字')
         res.status(400).json({
           status: 'failed',
-          message: '密碼不符合規則，需要包含英文數字大小寫，最短10個字，最長16個字'
+             message: '密碼不符合規則，需要包含英文數字大小寫，最短10個字，最長16個字'
         })
         return
       }
@@ -87,10 +87,10 @@ class UsersController {
         return
       }
       if (!passwordPattern.test(password)) {
-        logger.warn('密碼不符合規則，需要包含英文數字大小寫，最短10個字，最長16個字')
+          logger.warn('密碼不符合規則，需要包含英文數字大小寫，最短10個字，最長16個字')
         res.status(400).json({
           status: 'failed',
-          message: '密碼不符合規則，需要包含英文數字大小寫，最短10個字，最長16個字'
+           message: '密碼不符合規則，需要包含英文數字大小寫，最短10個字，最長16個字'
         })
         return
       }
@@ -270,10 +270,10 @@ class UsersController {
         return
       }
       if (!passwordPattern.test(password) || !passwordPattern.test(newPassword) || !passwordPattern.test(confirmNewPassword)) {
-        logger.warn('密碼不符合規則，需要包含英文數字大小寫，最短8個字，最長16個字')
+          logger.warn('密碼不符合規則，需要包含英文數字大小寫，最短10個字，最長16個字')
         res.status(400).json({
           status: 'failed',
-          message: '密碼不符合規則，需要包含英文數字大小寫，最短8個字，最長16個字'
+           message: '密碼不符合規則，需要包含英文數字大小寫，最短10個字，最長16個字'
         })
         return
       }
